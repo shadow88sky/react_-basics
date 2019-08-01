550a96b8c7e28bf1ff245c2b7b3ec84109352afe
shadow88sky <119136016@qq.com>
2019-08-01 10:06:16 +0800

    no message

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'; // 创建组件，虚拟DOM，生命周期
 import ReactDOM from 'react-dom'; // 创建好的组件和 虚拟DOM放到页面上展示  
 import LifeCycle from './components/lifeCycle'
 
-import BasicExample from './router/router'
+import AuthExample from './router/redirect'
 
 import {
     BrowserRouter as Router,
@@ -11,5 +11,5 @@ import {
 } from 'react-router-dom'
 
 ReactDOM.render(<div>
-    <BasicExample />
+    <AuthExample />
 </div>, document.getElementById('app'))
